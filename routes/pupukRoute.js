9ae2fc00ba62a1db5208605e2f2c8a9b9126a214
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-11-23 23:20:51 +0700

    menambahkan route untuk update data

--- routes/pupukRoute.js
+++ routes/pupukRoute.js
@@ -18,7 +18,18 @@ router.delete('/:kd_pupuk', (req, res) => {
     if (pupukIndex === -1) return res.status(404).json({ message: 'Data tidak ditemukan' });
 
     const deletedpupuk = pupuk.splice(pupukIndex, 1)[0];
-    res.status(200).json({ message: `Pupuk. '${deletedpupuk.nm_pupuk}' telah dihapus` });
+    res.status(200).json({ message: `Produk. '${deletedpupuk.nm_pupuk}' telah dihapus` });
 });
+
+router.put('/:kd_pupuk', (req, res) =>{
+    const pupuk_u = pupuk.find(p => p.kd_pupuk === parseInt(req.params.kd_pupuk));
+    if (!pupukUpdt) return res.status(404).json({ message: 'Data tidak ditemukan' });
+    pupukUpdt.nm_pupuk = req.body.nm_pupuk || pupukUpdt.nm_pupuk;
+    pupukUpdt.stock = req.body.stock || pupukUpdt.stock;
+
+   
+});
+
+
 
 export default router;
