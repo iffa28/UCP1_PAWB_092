4a63fcc16fb9a406962193bac5eb47af568a91ad
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-11-23 22:39:31 +0700

    menambahkan library yang digunakan

---
+++ index.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import PupukRoutes from "./routes/pupukRoute.js";
+import BibitRoutes from "./routes/bibitRoute.js";
+
+const app = express();
