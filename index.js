75e569c16233bdedafbd5eba9407a9905cfb2f59
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-11-23 23:10:18 +0700

    menambahkan route delete

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import PupukRoutes from "./routes/pupukRoute.js";
-import BibitRoutes from "./routes/bibitRoute.js";
+import pupukRoutes from "./routes/pupukRoute.js";
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use('/pupuk', pupukRoutes);
-app.use('/bibit', bibitRoutes);
 
 app.listen(3000, () => {
     console.log(`Server is running on http://localhost:${3000}`);
--- routes/pupukRoute.js
+++ routes/pupukRoute.js
@@ -4,7 +4,7 @@ const pupuk = [];
 
 router.get('/', (req, res) => { res.json(pupuk); });
 router.post('/', (req, res) => {
-    const newProduct = {
+    const newPupuk = {
         kd_pupuk: pupuk.length + 1,
         nm_pupuk: req.body.nm_pupuk,
         stock: req.body.stock,
@@ -12,3 +12,13 @@ router.post('/', (req, res) => {
     pupuk.push(newPupuk);
     res.status(201).json(newPupuk)
 });
+
+router.delete('/:kd_pupuk', (req, res) => {
+    const pupukIndex = pupuk.findIndex(p => p.kd_pupuk === parseInt(req.params.kd_pupuk));
+    if (pupukIndex === -1) return res.status(404).json({ message: 'Data tidak ditemukan' });
+
+    const deletedpupuk = pupuk.splice(pupukIndex, 1)[0];
+    res.status(200).json({ message: `Pupuk. '${deletedpupuk.nm_pupuk}' telah dihapus` });
+});
+
+export default router;
