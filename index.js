aea81f6210d8869429209dd608ab92a469ee7f7f
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-11-23 23:40:17 +0700

    menambahkan kode route delete berdasarkan kode

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import pupukRoutes from "./routes/pupukRoute.js";
-import bibitRoutes from "./routes/pupukRoute.js";
+import bibitRoutes from "./routes/bibitRoute.js";
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use('/pupuk', pupukRoutes);
 app.use('/bibit', bibitRoutes);
+app.use('/pupuk', pupukRoutes);
+
 
 app.listen(3000, () => {
     console.log(`Server is running on http://localhost:${3000}`);
--- routes/bibitRoute.js
+++ routes/bibitRoute.js
@@ -13,3 +13,19 @@ router.post('/', (req, res) => {
     bibit.push(newBibit); 
     res.status(201).json(newBibit); 
 });
+
+router.delete('/:kd_bibit', (req, res) => {
+    const bibitIndex = bibit.findIndex(b => b.kd_bibit === parseInt(req.params.kd_bibit)); 
+    if (bibitIndex === -1) { 
+        return res.status(404).json({ 
+            message: `Data bibit dengan kode '${req.params.kd_bibit}' tidak ditemukan` 
+        });
+    }
+    const deletedBibit = bibit.splice(bibitIndex, 1)[0]; 
+    res.status(200).json({ 
+        message: `Bibit '${deletedBibit.nm_bibit}' telah berhasil dihapus`, 
+        deletedBibit 
+    });
+});
+
+export default router;
