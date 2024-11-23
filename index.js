e3ad9058382211661e25c7c5d1c75907e9c312bd
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-11-23 22:45:44 +0700

    menggunakan library dan mengatur port

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ import PupukRoutes from "./routes/pupukRoute.js";
 import BibitRoutes from "./routes/bibitRoute.js";
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use('/mobil', pupukRoutes);
+app.use('/todos', bibitRoutes);
+
+app.listen(3000, () => {
+    console.log(`Server is running on http://localhost:${3000}`);
+});
