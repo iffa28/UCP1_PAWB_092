c2885a7d242dfeffaadeb2598bf7a1e2c341d3c2
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-11-23 22:53:59 +0700

    menambahkan router post untuk menambahkan data

--- routes/pupukRoute.js
+++ routes/pupukRoute.js
@@ -3,3 +3,12 @@ const router = express.Router();
 const pupuk = [];
 
 router.get('/', (req, res) => { res.json(pupuk); });
+router.post('/', (req, res) => {
+    const newProduct = {
+        kd_pupuk: pupuk.length + 1,
+        nm_pupuk: req.body.nm_pupuk,
+        stock: req.body.stock,
+    };
+    pupuk.push(newPupuk);
+    res.status(201).json(newPupuk)
+});
