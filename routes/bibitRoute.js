df432b8df0461cde37d33748f061ca88b1c3314c
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-11-23 23:26:48 +0700

    menambahkan post bibit

--- routes/bibitRoute.js
+++ routes/bibitRoute.js
@@ -3,3 +3,13 @@ const router = express.Router();
 const bibit = [];
 
 router.get('/', (req, res) => { res.json(bibit); });
+
+router.post('/', (req, res) => {
+    const newBibit = {
+        kd_bibit: bibit.length + 1,
+        nm_bibit: req.body.nm_bibit, 
+        stock: req.body.stock, 
+    };
+    bibit.push(newBibit); 
+    res.status(201).json(newBibit); 
+});
